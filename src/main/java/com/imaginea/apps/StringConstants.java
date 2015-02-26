8e53f564dde313cecf12c7ef59b89410f8f22e05
vamsi-emani <vamsi2203@gmail.com>
2015-02-26 12:30:23 +0530

    Externalized string info, error and warn msgs.

--- src/main/java/com/imaginea/apps/StringConstants.java
+++ src/main/java/com/imaginea/apps/StringConstants.java
@@ -9,4 +9,11 @@ public class StringConstants {
 
 	public static final String BASEURL = "http://mail-archives.apache.org/mod_mbox/maven-users/";
 	
+	public static final String CANNOT_CRAWL = "Unable to run the crawler. Please verify inputs or check your internet connection.";
+	
+	public static final String NUM_DOWNLOAD_WORKERS = "Enter number download worker threads :";
+	
+	public static final String INVALID_NUM_WORKERS = "Invalid input, default download worker thread count set to 1";
+	
+	
 }
