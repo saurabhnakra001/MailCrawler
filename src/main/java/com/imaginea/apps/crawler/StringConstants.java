cd30f0291f6229a555dce55cfcd2479e84243e61
vamsi-emani <vamsi2203@gmail.com>
2015-03-04 10:12:04 +0530

    Exception handling for "no internet connection".

--- src/main/java/com/imaginea/apps/crawler/StringConstants.java
+++ src/main/java/com/imaginea/apps/crawler/StringConstants.java
@@ -9,7 +9,7 @@ public class StringConstants {
 
 	public static final String BASEURL = "http://mail-archives.apache.org/mod_mbox/maven-users/";
 	
-	public static final String CANNOT_CRAWL = "Unable to run the crawler. Please verify inputs or check your internet connection.";
+	public static final String CANNOT_CRAWL = "Unable to run the crawler.";
 	
 	public static final String NUM_DOWNLOAD_WORKERS = "Enter number of download worker threads :";
 	
@@ -20,6 +20,8 @@ public class StringConstants {
 	public static final String INPUT_YEAR = "2014";
 
 	public static final String NUM_LINK_GENERATE_WORKERS = "Enter number of link generate worker threads : ";
+
+	public static final String CHECK_INTERNET_CONNECTION = "Please check your internet connection";
 	
 	
 }
